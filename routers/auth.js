3bee82204163de4d66b989baf870b713f042964c
nisabmohd <nisabmohd@gmail.com>
2023-07-16 11:40:06 +0530

    Migrated to NextJS 13

--- routers/auth.js
+++
@@ -1,9 +0,0 @@
-const { login, signup } = require("../controllers/auth");
-
-const router = require("express").Router();
-
-router.route("/login").post(login);
-
-router.route("/signup").post(signup);
-
-module.exports = router;
--- routers/chat.js
+++
@@ -1,8 +0,0 @@
-const { chat, getAllChats } = require("../controllers/chat");
-const { checkApiKey } = require("../middlewares/apiKey");
-const router = require("express").Router();
-
-router.route("/chat/:apiKey").post(checkApiKey, chat);
-router.route("/getchats/:apiKey").get(checkApiKey, getAllChats);
-
-module.exports = router;
